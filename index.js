a960a10450c914d5cd7f84aeac28228ee1e102e3
catpea <corporate.miou@gmail.com>
2025-08-19 18:46:57 -0400

    Updated Release

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-import { Offcanvas } from 'offcanvas/Component.js';
+import { OffcanvasPanel } from "#offcanvas-panel"; // installed, now you can use <webkin-offcanvas>
+import { OutlineButton } from "#outline-button"; // installed, now you can use <webkin-offcanvas>
+import { ThemeCustomizer } from "#theme-customizer"; // installed, now you can use <webkin-offcanvas>
+// import ladybits from '#ladybits';
 
-export default { Offcanvas }
+export default { OffcanvasPanel, OutlineButton, ThemeCustomizer };
